7b9611e0bde12de2b3b7efa70c3ffe6370149059
Szilard Csiki <szilard.csiki@ge.com>
2023-09-27 19:39:37 +0200

    add service methods for save new beer and update beer

--- src/main/java/com/r0nuh/udemymicroservicebrewery/services/BeerService.java
+++ src/main/java/com/r0nuh/udemymicroservicebrewery/services/BeerService.java
@@ -5,4 +5,8 @@ import java.util.UUID;
 import com.r0nuh.udemymicroservicebrewery.web.model.BeerDTO;
 public interface BeerService {
     BeerDTO getBeerById(UUID beerId);
+
+    BeerDTO saveNewBeer(BeerDTO beerDTO);
+
+    void updateBeer(UUID beerId, BeerDTO beerDTO);
 }
--- src/main/java/com/r0nuh/udemymicroservicebrewery/services/BeerServiceImpl.java
+++ src/main/java/com/r0nuh/udemymicroservicebrewery/services/BeerServiceImpl.java
@@ -10,9 +10,22 @@ import com.r0nuh.udemymicroservicebrewery.web.model.BeerDTO;
 public class BeerServiceImpl implements BeerService {
     @Override
     public BeerDTO getBeerById(UUID beerId) {
-    return BeerDTO.builder().id(UUID.randomUUID())
-            .beerName("Galaxy Cat")
-            .beerStyle("Pale Ale")
-            .build();
+        return BeerDTO.builder()
+                .id(UUID.randomUUID())
+                .beerName("Galaxy Cat")
+                .beerStyle("Pale Ale")
+                .build();
+    }
+
+    @Override
+    public BeerDTO saveNewBeer(BeerDTO beerDTO) {
+        return BeerDTO.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDTO beerDTO) {
+        //todo implement method
     }
 }
