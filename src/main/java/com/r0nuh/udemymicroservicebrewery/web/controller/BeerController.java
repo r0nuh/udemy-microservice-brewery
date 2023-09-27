d1048a7e33c1c4aeceb9e69f0908b8532d481645
Szilard Csiki <szilard.csiki@ge.com>
2023-09-27 19:41:08 +0200

    add Post (save new beer) and Put (update beer) method

--- src/main/java/com/r0nuh/udemymicroservicebrewery/web/controller/BeerController.java
+++ src/main/java/com/r0nuh/udemymicroservicebrewery/web/controller/BeerController.java
@@ -2,10 +2,14 @@ package com.r0nuh.udemymicroservicebrewery.web.controller;
 
 import java.util.UUID;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,4 +32,23 @@ public class BeerController {
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
     }
 
+    @PostMapping // create new beer
+    public ResponseEntity newBeer(@RequestBody BeerDTO beerDTO) {
+
+        BeerDTO savedDto = beerService.saveNewBeer(beerDTO);
+
+        HttpHeaders headers = new HttpHeaders();
+        //todo add hostname to url
+        headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
+
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity handleUpdate(@PathVariable UUID beerId, @RequestBody BeerDTO beerDTO) {
+
+        beerService.updateBeer(beerId, beerDTO);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
